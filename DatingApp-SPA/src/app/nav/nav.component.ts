153b72377d4cdaf18c9b89a2e7231d803fe0b7c7
corp\negin.salamat <negin.salamat@futura-consulting.net>
2019-12-19 15:42:45 +0100

    End of Section 7

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "../_services/auth.service";
 import { AlertifyService } from "../_services/alertify.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-nav",
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router
   ) {}
 
   ngOnInit() {}
@@ -27,6 +29,9 @@ export class NavComponent implements OnInit {
       error => {
         // console.log(error);
         this.alertify.error(error);
+      },
+      () => {
+        this.router.navigate(["./members"]);
       }
     );
   }
@@ -41,5 +46,6 @@ export class NavComponent implements OnInit {
     localStorage.removeItem("token");
     //console.log("logged out");
     this.alertify.message("logged out");
+    this.router.navigate(["./home"]);
   }
 }
